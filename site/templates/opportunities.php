1eb30acda77d6a67511b0b6ac2a7c9ab9d40f3ac
Taylor Hokanson <info@taylorhokanson.com>
2024-09-13 09:06:33 -0500

    little more css to get Rob going on opportunities

--- site/templates/opportunities.php
+++ site/templates/opportunities.php
@@ -5,29 +5,26 @@
 <?php snippet('navigation') ?>
 
 <!-- content section -->
-<main class="opportunities">
-  <!-- Grab title text from txt field -->
-  <h3><?= $page->title() ?></h3>
-
-    <ul>
+<main class="all_opps">
+    <!-- grab title from opportunities.txt -->
+    <h3><?= $page->title() ?></h3>
+    <ul class="one_opp">
         <!-- grab all children folders and list them -->
         <?php foreach ($page->children()->listed() as $opportunity): ?>
-        <li>
+        <li class="one_opp">
             <a href="<?= $opportunity->url() ?>">
-            
-                <!-- grab first image in project folder (alphabetically) -->
-                <?= $opportunity->image()->crop(100, 100, 100) ?>
-                <div id="container">
-                    <ul>
-                        <li><?= $opportunity->Title() ?></li>
-                        <li><?= $opportunity->Description() ?></li>
-                        <li><?= $opportunity->Due() ?></li>
-                    </ul>
-                </a>
-            </li>
+                <ul>
+                    <!-- grab first image in project folder (alphabetically) -->
+                    <li><?= $opportunity->image()->crop(100, 100, 100) ?></li>
+                    <!-- pull from sections defined in each opportunity text file -->
+                    <li><?= $opportunity->Title() ?></li>
+                    <li><?= $opportunity->Description() ?></li>
+                    <li><?= $opportunity->Due() ?></li>
+                </ul>
+             </a>
+        </li>
         <?php endforeach ?> 
     </ul>
-
 </main>
 
 <!-- bottom section -->

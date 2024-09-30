1938797a1dee92021b5324c4feb970f990baab72
Rob Ray <47587146+shimmeringtrashpile@users.noreply.github.com>
2024-09-30 08:28:51 -0700

    Grid layout work. Desktop layout is busted.

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -7,5 +7,5 @@
   <!-- nav section -->
 <?php snippet('navigation') ?>
 
-  <span class="blurb">The New Media Caucus (NMC) is an international non-profit association formed to promote the development and understanding of new media art. We represent and serve: artists, designers, practitioners, historians, theorists, educators, students, and scholars. <a href="">Join us</a>.</span>
+  <div class="blurb">The New Media Caucus (NMC) is an international non-profit association formed to promote the development and understanding of new media art. We represent and serve: artists, designers, practitioners, historians, theorists, educators, students, and scholars. <a href="">Join us</a>.</div>
 </footer>
--- site/templates/opportunities.php
+++ site/templates/opportunities.php
@@ -7,26 +7,32 @@
 <!-- content section -->
 <main>
     <!-- grab title from opportunities.txt -->
-    <div class="opportunities">
+    <div class="opportunities-title">
         <h1><?= $page->title() ?></h1>
     </div>
-    <!-- grab all children folders and list them -->
-    <?php foreach ($page->children()->listed() as $opportunity): ?>
-        <div class="post-image">
-            <a href="<?= $opportunity->url() ?>">
-                <!-- grab first image in project folder (alphabetically) -->
-                <?= $opportunity->image()->crop(320, 320, 320) ?>                  
-            </a>
+    <div class="opportunities">
+        <!-- grab all children folders and list them -->
+        <div class="opportunity-row">
+            <?php foreach ($page->children()->listed() as $opportunity): ?>
+            <div class="opportunity">
+                <div class="post-image">
+                    <a href="<?= $opportunity->url() ?>">
+                        <!-- grab first image in project folder (alphabetically) -->
+                        <?= $opportunity->image()->crop(320, 320, 320) ?>                  
+                    </a>
+                </div>
+                <div class="post">
+                    <a href="<?= $opportunity->url() ?>">
+                        <!-- pull from sections defined in each opportunity text file -->
+                        <h2><?= $opportunity->Title() ?></h2>                    
+                    </a>
+                    <p class="description"><?= $opportunity->Description() ?></p>
+                    <p class="due-date">DEADLINE: <?= $opportunity->Due() ?><p class="due-date">
+                </div>
+            </div>
+            <?php endforeach ?> 
         </div>
-        <div class="post">
-            <a href="<?= $opportunity->url() ?>">
-                <!-- pull from sections defined in each opportunity text file -->
-                <h2><?= $opportunity->Title() ?></h2>                    
-            </a>
-            <p class="description"><?= $opportunity->Description() ?></p>
-            <p class="due-date">DEADLINE: <?= $opportunity->Due() ?><p class="due-date">
-        </div>
-    <?php endforeach ?> 
+    </div>
 </main>
 
 <!-- bottom section -->

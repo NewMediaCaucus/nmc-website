e64bc2fcefdc2c24320b566d643ba3e148297031
Rob Ray <47587146+shimmeringtrashpile@users.noreply.github.com>
2024-09-29 18:53:03 -0700

    Add construction worker favicon and current logo. Start roughing in media queries and CSS grid work.

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -1,4 +1,12 @@
 <footer class="footer">
+<div class="logo-img">
+    <img class="logo-img" src="<?= url('logo.png') ?>" alt="" width="150" height="44">
+  </div>
+  <a class="logo" href="<?= $site->url() ?>"><?= $site->title() ?></a>
+  
+<span class="blurb">Promoting the development and understanding of new media art.</span>
+
+
 <!-- nav section -->
 <?php snippet('navigation') ?>
 </footer>
--- site/templates/opportunities.php
+++ site/templates/opportunities.php
@@ -8,21 +8,23 @@
 <main class="opportunities">
     <!-- grab title from opportunities.txt -->
     <h1><?= $page->title() ?></h1>
-    <ul class="opportunities-list">
+    <div class="opportunities-list">
         <!-- grab all children folders and list them -->
         <?php foreach ($page->children()->listed() as $opportunity): ?>
         <section class="opportunity">
             <a href="<?= $opportunity->url() ?>">
-                    <!-- grab first image in project folder (alphabetically) -->
-                    <?= $opportunity->image()->crop(100, 100, 100) ?>
-                    <!-- pull from sections defined in each opportunity text file -->
-                    <h2><?= $opportunity->Title() ?></h2>                    
-                </a>
+                <!-- grab first image in project folder (alphabetically) -->
+                <?= $opportunity->image()->crop(450, 450, 450) ?>                  
+            </a>
+            <a href="<?= $opportunity->url() ?>">
+                <!-- pull from sections defined in each opportunity text file -->
+                <h2><?= $opportunity->Title() ?></h2>                    
+            </a>
                 <p class="description"><?= $opportunity->Description() ?></p>
-                <p class="due-date">DUE DATE: <?= $opportunity->Due() ?><p class="due-date">
+                <p class="due-date">DEADLINE: <?= $opportunity->Due() ?><p class="due-date">
         </section>
         <?php endforeach ?> 
-    </ul>
+    </div>
 </main>
 
 <!-- bottom section -->

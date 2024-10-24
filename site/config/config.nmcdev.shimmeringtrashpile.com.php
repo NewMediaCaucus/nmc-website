d598b0b769373412a93691eb43851a157c2114c6
Rob Ray <47587146+shimmeringtrashpile@users.noreply.github.com>
2024-10-24 11:37:47 -0700

    Add config files for multi-environment setup.

---
+++ site/config/config.nmcdev.shimmeringtrashpile.com.php
@@ -0,0 +1,14 @@
+<?php
+
+# added by THO
+
+# disable panel creation now that we've already got one
+#return [
+#    'panel' => false
+#  ];
+
+return [
+    'panel' =>[
+      'install' => true
+    ]
+  ];

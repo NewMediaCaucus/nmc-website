34c017377656ca60f7844b2f2c5c61aaae140bad
Rob Ray <47587146+shimmeringtrashpile@users.noreply.github.com>
2024-10-25 15:22:00 -0700

    restack nav to the top

--- site/templates/opportunities.php
+++ site/templates/opportunities.php
@@ -1,8 +1,9 @@
+<!-- nav section -->
+<?php snippet('navigation') ?>
+
 <!-- top section -->
 <?php snippet('header') ?>
 
-<!-- nav section -->
-<?php snippet('navigation') ?>
 
 <!-- content section -->
 <main>

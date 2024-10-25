e3376ef54adfa02c77f216f25d955d4faa21a4fb
Rob Ray <47587146+shimmeringtrashpile@users.noreply.github.com>
2024-10-25 16:17:03 -0700

    scale down social icons

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -25,12 +25,12 @@
   <div class="footer-social">
     <div class="social-icon">
       <a href="https://www.facebook.com/groups/newmediacaucus" target="_blank">
-        <img src="<?= url('fb.png') ?>" alt="Facebook" width="24" height="24">
+        <img src="<?= url('fb.png') ?>" alt="Facebook" width="18" height="18">
       </a>
     </div>
     <div class="social-icon">
       <a href="https://www.instagram.com/newmediacaucus/" target="_blank">
-        <img src="<?= url('ig.png') ?>" alt="Instagram" width="24" height="24">
+        <img src="<?= url('ig.png') ?>" alt="Instagram" width="18" height="18">
       </a>
     </div>
     <div class="copyright">Copyright © 2024</div>

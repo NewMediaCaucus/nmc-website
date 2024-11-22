ce3f3c3cfd07aead00eaf88027f29d3347dccc99
Rob Ray <47587146+shimmeringtrashpile@users.noreply.github.com>
2024-11-21 23:39:38 -0800

    Add additional styling. Bugfix y to ies for entry to entries.

--- site/templates/about.php
+++ site/templates/about.php
@@ -11,23 +11,15 @@
     <div class="about-title">
         <h1><?= $page->title() ?></h1>
     </div>
-    <p><?= $page->mission() ?></p>
+    <p><?= $page->mission()->kirbytext() ?></p>
     <?php
+        // Setup filters for each category
         $humans = $page->children()->listed();
-        
-        // Filter the humans by category 'officer'
         $officers = $humans->filterBy('category', 'officer');
-        
-        // Filter the humans by category 'board'
         $board_members = $humans->filterBy('category', 'board');
-        
-        // Filter the humans by category 'advisory board'
         $advisory_members = $humans->filterBy('category', 'advisory');
-        
-        // Filter the humans by category 'alumni'
         $alumni = $humans->filterBy('category', 'alumni');
-        
-        ?>
+    ?>
 
     <h2>Officers</h2>
     <!-- List Officers -->
@@ -40,25 +32,21 @@
                         <a href="<?= $officer->url() ?>">
                             <!-- grab first image in project folder (alphabetically) -->
                             <img src="<?= $image->crop(468)->url() ?>" alt="<?= $officer->alt() ?>">
-
                         </a>
                     </div>
                     <div class="post">
                         <h3><?= $officer->Title() ?></h3>                     
-                        <!-- Display NMC Title -->
                         <?php if ($officer->nmc_title()->isNotEmpty()): ?>
                             <p class="nmc_title"><?= $officer->nmc_title() ?></p>
                         <?php endif ?>
-                        <!-- Display Professional Title -->
                         <?php if ($officer->professional_title()->isNotEmpty()): ?>
                             <p class="professional_title"><?= $officer->professional_title() ?></p>
                         <?php endif ?>
-                        <!-- Display short description -->
                         <p class="description"><?= $officer->short_description() ?></p>
                         <!-- Read More Button -->
-                        <a href="<?= $officer->url() ?>" class="button">
+                        <!-- <a href="<?= $officer->url() ?>" class="button">
                             View Bio
-                        </a>
+                        </a> -->
                     </div>
                 </div>
             <?php endif ?>              
@@ -80,16 +68,14 @@
                     </div>
                     <div class="post">
                         <h3><?= $board_member->Title() ?></h3>                     
-                        <!-- Display Professional Title -->
                         <?php if ($board_member->professional_title()->isNotEmpty()): ?>
                             <p class="professional_title"><?= $board_member->professional_title() ?></p>
                         <?php endif ?>
-                        <!-- Display short description -->
                         <p class="description"><?= $board_member->short_description() ?></p>
                         <!-- Read More Button -->
-                        <a href="<?= $board_member->url() ?>" class="button">
+                        <!-- <a href="<?= $board_member->url() ?>" class="button">
                             View Bio
-                        </a>
+                        </a> -->
                     </div>
                 </div>
             <?php endif ?>              
@@ -111,11 +97,9 @@
                     </div>
                     <div class="post">
                         <h3><?= $advisory_member->Title() ?></h3>                     
-                        <!-- Display Professional Title -->
                         <?php if ($advisory_member->professional_title()->isNotEmpty()): ?>
                             <p class="professional_title"><?= $advisory_member->professional_title() ?></p>
                         <?php endif ?>
-                        <!-- Display short description -->
                         <p class="description"><?= $advisory_member->short_description() ?></p>
                         <!-- Read More Button -->
                         <a href="<?= $advisory_member->url() ?>" class="button">
@@ -129,9 +113,7 @@
 
     <h2>Board Alumni & Past Leadership</h2>
     <!-- List Board Members -->
-    <div class="about">
-    <p><?= $page->alumni() ?></p>
-    </div>
+    <p><?= $page->alumni()->kirbytext() ?></p>
 
 <!-- Submit an opportunity promo -->
 <div class="promo">🪩 <a href="https://www.newmediacaucus.org/join/" >Become a Member!</a> 🐝</div>

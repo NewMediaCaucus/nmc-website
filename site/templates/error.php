df625c1b24b53dae91f95af769adbd51c1c4b990
Rob Ray <47587146+shimmeringtrashpile@users.noreply.github.com>
2025-04-20 16:10:41 -0700

    Add 404 error template and animate gif.

---
+++ site/templates/error.php
@@ -0,0 +1,21 @@
+<!-- top section -->
+<?php snippet('header') ?>
+
+<!-- nav section -->
+<?php snippet('navigation') ?>
+
+<!-- content section -->
+<main>
+  <!-- Grab title text from txt field -->
+  <img class="post-image"
+    src="assets/icons/404-960.gif"
+    alt="Animated 404 error"
+    width="480px">
+  <h1>Oops! This page doesn’t exist.</h1>
+  <p>We recently gave our website a fresh new look, so some pages may have moved or been retired.</p>
+  <p>If you are looking for something specific, try heading back to the <a href="http://newmediacaucus.org"> home page</a> or using the site navigation to find what you're looking for.</p>
+</main>
+
+
+<!-- bottom section -->
+<?php snippet('footer') ?>

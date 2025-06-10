e1378627a4a7ab2c04ca0c213a420d2862d30152
Taylor Hokanson <info@taylorhokanson.com>
2025-06-10 08:46:45 -0500

    Update config.php

--- site/config/config.php
+++ site/config/config.php
@@ -1,14 +1,8 @@
 <?php
 
+# Note that some (all?) settings in this vanilla config file will be overwritten
+# by the domain-specific config files in the same directory
+
 return [
-    'routes' => [
-        [
-            # template for url redirects
-            # leading slash is optional
-            'pattern' => 'redirect/test',
-            'action' => function () {
-                go('opportunities');
-            }
-        ],
-    ]
+
 ];
